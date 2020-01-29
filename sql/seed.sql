153ce59a444460728b16bbe58cbff9fba8f66ac1
Kevin Geary <kgeary@gmail.com>
2020-01-29 15:57:05 -0600

    Renamed create.sql to schema.sql, Cleaned up seed.sql removing unnecessary text

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,35 +1,48 @@
 USE employee_trackerDB;
 
 --- Seed Departments ---
-INSERT INTO Department (name) VALUES ("Accounting");
-INSERT INTO Department (name) VALUES ("Business Development");
-INSERT INTO Department (name) VALUES ("Engineering");
-INSERT INTO Department (name) VALUES ("Human Resources");
-INSERT INTO Department (name) VALUES ("Information Technology");
+INSERT INTO
+  Department (name)
+VALUES
+  ("Accounting"),
+  ("Business Development"),
+  ("Engineering"),
+  ("Human Resources"),
+  ("Information Technology");
 
 --- Seed Roles ---
-INSERT INTO Role (title, salary, department_id) values ("Jr. Accountant", 50000, 1);
-INSERT INTO Role (title, salary, department_id) values ("Sr. Accountant", 100000, 1);
-INSERT INTO Role (title, salary, department_id) values ("Manager of Accounting", 100000, 1);
-INSERT INTO Role (title, salary, department_id) values ("Business Development Associate", 60000, 2);
-INSERT INTO Role (title, salary, department_id) values ("Manager of Development", 150000, 2);
-INSERT INTO Role (title, salary, department_id) values ("Engineer 1", 50000, 3);
-INSERT INTO Role (title, salary, department_id) values ("Engineer 2", 75000, 3);
-INSERT INTO Role (title, salary, department_id) values ("Engineer 3", 100000, 3);
-INSERT INTO Role (title, salary, department_id) values ("Manager of Engineering", 150000, 3);
-INSERT INTO Role (title, salary, department_id) values ("IT Help Desk", 40000, 4);
-INSERT INTO Role (title, salary, department_id) values ("IT Specialist", 70000, 4);
-INSERT INTO Role (title, salary, department_id) values ("IT Manager", 120000, 4);
+INSERT INTO
+  Role (title, salary, department_id)
+VALUES
+  ("Jr. Accountant", 50000, 1),
+  ("Sr. Accountant", 100000, 1),
+  ("Manager of Accounting", 100000, 1),
+  ("Business Development Associate", 60000, 2),
+  ("Manager of Development", 150000, 2),
+  ("Engineer 1", 50000, 3),
+  ("Engineer 2", 75000, 3),
+  ("Engineer 3", 100000, 3),
+  ("Manager of Engineering", 150000, 3),
+  ("IT Help Desk", 40000, 4),
+  ("IT Specialist", 70000, 4),
+  ("IT Manager", 120000, 4);
 
 --- Seed Employees
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Adam", "Anderson", 3, null);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Bill", "Bower", 1, 1);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Charlie", "Cats", 9, null);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Doug", "Dewey", 6, 3);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Ed", "Eerie", 6, 1);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Fred", "Funk", 7, 2);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Gary", "Gulp", 7, 2);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Hank", "Henry", 8, 2);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Isaac", "Irwin", 4, 1);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id) values ("Jarvis", "James", 4, 1);
+INSERT INTO
+  Employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ("Adam", "Anderson", 3, null),
+  ("Bill", "Bower", 1, 1),
+  ("Charlie", "Cats", 9, null),
+  ("Doug", "Dewey", 6, 3),
+  ("Ed", "Eerie", 6, 1),
+  ("Fred", "Funk", 7, 2),
+  ("Gary", "Gulp", 7, 2),
+  ("Hank", "Henry", 8, 2),
+  ("Isaac", "Irwin", 4, 1),
+  ("Jarvis", "James", 4, 1);
 
+SELECT
+  *
+FROM
+  Employee
