f16c3583cc31af9a5548c67d47deccbc7cab2321
Kevin Geary <kgeary@gmail.com>
2020-01-30 17:07:12 -0600

    Updated schema.sql with indexes for fk. Updated tracker to show view after all modifications

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,28 +1,39 @@
 -- Drop Existing Database, Then Create New Database --
 DROP DATABASE IF EXISTS employee_trackerDB;
+
 CREATE DATABASE employee_trackerDB;
+
 USE employee_trackerDB;
 
 CREATE TABLE Department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE
 );
 
 CREATE TABLE Role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE,
-  salary DECIMAL(10,2),
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES Department(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE,
+    salary DECIMAL(10 , 2 ),
+    department_id INT,
+    INDEX department_index (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id)
+        REFERENCES Department (id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE Employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES Role(id),
-  FOREIGN KEY (manager_id) REFERENCES Employee(id),
-  UNIQUE(first_name, last_name)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    INDEX role_index (role_id),
+    INDEX manager_index (manager_id),
+    UNIQUE (first_name , last_name),
+    CONSTRAINT fk_role FOREIGN KEY (role_id)
+        REFERENCES Role (id)
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id)
+        REFERENCES Employee (id)
+        ON DELETE SET NULL
 );
